9a5175065314b349f071fe767d559ab4f78377bd
helehh <kuulmets@gmail.com>
2023-11-20 10:44:16 +0200

    fix & move dataset classes

--- batch_instruction_inference.py
+++ batch_instruction_inference.py
@@ -9,84 +9,12 @@ import sys
 import json
 
 import torch
-from datasets import ValidationDataset, EstQADataset
+from datasets import ValidationDataset, EstQADataset, ChatDataset, InstructionDataset
 import torch.distributed._shard.checkpoint as dist_cp
 from torch.distributed.checkpoint import FileSystemReader
 from tqdm import tqdm
 
 from transformers import LlamaTokenizer, LlamaForCausalLM, LlamaConfig
-
-PROMPT_DICT = {
-    "prompt_input": (
-        "Below is an instruction that describes a task, paired with an input that provides further context. "
-        "Write a response that appropriately completes the request.\n\n"
-        "### Instruction:\n{instruction}\n\n### Input:\n{input}\n\n### Response:\n"
-    ),
-    "prompt_no_input": (
-        "Below is an instruction that describes a task. "
-        "Write a response that appropriately completes the request.\n\n"
-        "### Instruction:\n{instruction}\n\n### Response:\n"
-    ),
-}
-
-
-class InstructionDataset(Dataset):
-    def __init__(self, data):
-        self.data = data
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, index):
-        item = self.data[index]
-
-        if item.get("input", "") == "":
-            prompt = PROMPT_DICT["prompt_no_input"].format_map(item)
-        else:
-            prompt = PROMPT_DICT["prompt_input"].format_map(item)
-
-        if "output" not in item:
-            return {"prompts": prompt}
-
-        return {"prompts": prompt, "labels": item["output"]}
-
-class ChatDataset(Dataset):
-    SYSTEM_PREFIX = "<|system|>\n"
-    SYSTEM_SUFFIX = "\n"
-    ASSISTANT_PREFIX = "<|assistant|>\n"
-    ASSISTANT_SUFFIX = "\n"
-    USER_PREFIX = "<|user|>\n"
-    USER_SUFFIX = "\n"
-
-    def __init__(self, data, tokenizer):
-        self.dataset = data
-        self.tokenizer = tokenizer
-
-    def __len__(self):
-        return len(self.dataset)
-
-    def _concat_messages(self, messages):
-        message_text = ""
-        for message in messages:
-            if message["role"] == "system":
-                message_text += self.SYSTEM_PREFIX + message["content"].strip() + self.SYSTEM_SUFFIX
-            elif message["role"] == "user":
-                message_text += self.USER_PREFIX + message["content"].strip() + self.USER_SUFFIX
-            elif message["role"] == "assistant":
-                message_text += self.ASSISTANT_PREFIX + message["content"].strip() + self.tokenizer.eos_token \
-                                + self.ASSISTANT_SUFFIX
-            else:
-                raise ValueError("Invalid role: {}".format(message["role"]))
-        return message_text
-
-    def __getitem__(self, index):
-        messages = self.dataset[index]["messages"]
-        if len(messages) == 0:
-            raise ValueError('messages field is empty.')
-
-        example_text = self._concat_messages(messages) + self.ASSISTANT_PREFIX
-        return {"prompts": example_text}
-
 
 def write_json(instructions: List[Dict[str, str]], file_path: str):
     with open(file_path, "w", encoding="utf-8") as f:
@@ -166,7 +94,7 @@ def main(
                 load_in_8bit=quantization,
                 device_map="auto",
                 return_dict=True,
-                low_cpu_mem_usage=True
+                low_cpu_mem_usage=True)
 
     else:
         logging.info("Loading sharded model")
